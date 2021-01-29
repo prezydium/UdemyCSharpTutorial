6c7c04b20e1b5aa468a4f65041aa9c14b4a85298
prezydium <presidium.dev@gmail.com>
2021-01-29 13:00:06 +0100

    Initial Commit

---
+++ Exercises/ControlFlow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercises
+{
+    public class ControlFlow
+    {
+        public static void CheckNumber()
+        {
+            Console.WriteLine("Enter number between 1 and 10. Don't try anything funny");
+            var input = Console.ReadLine();
+            var number = int.Parse(input);
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid!");
+            }
+        }
+    }
+}
