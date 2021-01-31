c96eb46ad0cb6bc2e69064f7891f697d51f64cb7
prezydium <presidium.dev@gmail.com>
2021-01-31 13:21:23 +0100

    Unique numbers exercise

---
+++ Exercises/CollectionsExercises/UniqueNumberDisplayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises.CollectionsExercises
+{
+    public class UniqueNumberDisplayer
+    {
+        public static void displayUnique()
+        {
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Enter 5 unique numbers");
+            for (int i = 0; i < 5; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (numbers.Contains(input))
+                {
+                    Console.WriteLine("Not unique! Try again");
+                    i--;
+                }
+                else
+                {
+                    numbers.Add(input);
+                }
+            }
+            Console.WriteLine("Unique numbers entered: " + string.Join(",", numbers));
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -6,7 +6,7 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            NameReverser.askForNameAndReverse();
+            UniqueNumberDisplayer.displayUnique();
         }
     }
 }
